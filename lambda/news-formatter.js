1aa02675d8b656a602836271c5cab09cd9a7f9bb
yamilmedina <yamilmedina@users.noreply.github.com>
2023-04-11 01:28:10 +0200

    fix: more code

--- lambda/news-formatter.js
+++ lambda/news-formatter.js
@@ -5,11 +5,11 @@ const DATE_FORMAT_OPTIONS = { weekday: 'long', month: 'long', day: 'numeric' };
 const formatNewsForTTS = function (news) {
     return news
         .map(article => {
-            "El " + new Date(article.pubDate).toLocaleDateString('es-CL', options)
-                + TTS_PAUSE_HEADLINE
-                + article.title
-                + TTS_PAUSE_HEADLINE
-                + article.description;
+            "El ".concat(new Date(article.date).toLocaleDateString('es-CL', DATE_FORMAT_OPTIONS))
+                .concat(TTS_PAUSE_HEADLINE)
+                .concat(article.title)
+                .concat(TTS_PAUSE_HEADLINE)
+                .concat(article.description);
         })
         .join(TTS_PAUSE_BETWEEN_ARTICLE);
 }
