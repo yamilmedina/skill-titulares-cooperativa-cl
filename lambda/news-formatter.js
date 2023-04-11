7987984ec66a80c8da450670027f684780fde02c
yamilmedina <yamilmedina@users.noreply.github.com>
2023-04-11 21:04:58 +0200

    chore: formatter

--- lambda/news-formatter.js
+++ lambda/news-formatter.js
@@ -3,15 +3,15 @@ const TTS_PAUSE_BETWEEN_ARTICLE = '<break time="3s"/>';
 const DATE_FORMAT_OPTIONS = { weekday: 'long', month: 'long', day: 'numeric' };
 
 const formatNewsForTTS = function (news) {
-    return news
-        .map(article => {
-            return 'El '.concat(new Date(article.date).toLocaleDateString('es-CL', DATE_FORMAT_OPTIONS))
-                .concat(TTS_PAUSE_HEADLINE)
-                .concat(article.title)
-                .concat(TTS_PAUSE_HEADLINE)
-                .concat(article.description)
-                .concat(TTS_PAUSE_BETWEEN_ARTICLE)
-        }).join();
+    let formattedNews = news.map(article => {
+        return 'El '.concat(new Date(article.date).toLocaleDateString('es-CL', DATE_FORMAT_OPTIONS))
+            .concat(TTS_PAUSE_HEADLINE)
+            .concat(article.title)
+            .concat(TTS_PAUSE_HEADLINE)
+            .concat(article.description)
+            .concat(TTS_PAUSE_BETWEEN_ARTICLE)
+    }).join();
+    return formattedNews;
 }
 
 module.exports = { formatNewsForTTS }
