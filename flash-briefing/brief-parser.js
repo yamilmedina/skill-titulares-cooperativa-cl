96efd02d3e7e6586f1b599c7474915b5c4c28c85
yamilmedina <yamilmedina@users.noreply.github.com>
2023-04-26 23:35:36 +0200

    feat: base for brief

---
+++ flash-briefing/brief-parser.js
@@ -0,0 +1,27 @@
+const { XMLParser } = require('fast-xml-parser');
+const axios = require('axios');
+const jsonNormalize = require('json-normalize');
+const short = require('short-uuid');
+
+// yyyy-MM-dd’T’HH:mm:ss'.0Z'
+const parser = new XMLParser();
+
+function parseNewsToAlexaBrief() {
+  axios.get('https://www.cooperativa.cl/noticias/site/tax/port/all/rss____1.xml')
+    .then((result) => {
+      const jsonObj = parser.parse(result.data);
+      const processedNews = jsonObj.rss.channel.item
+        .filter((it) => it.description !== '')
+        .map((it) => ({
+          uid: short.uuid(),
+          titleText: it.title,
+          mainText: it.prontus_audio1 === undefined ? it.description : '',
+          updateDate: new Date(it.pubDate).toISOString(),
+          redirectionUrl: it.link,
+          streamUrl: it.prontus_audio1 !== undefined ? `https://media.cooperativa.cl${it.prontus_audio1}` : '',
+        }));
+      jsonNormalize.stringify(processedNews, (err, results) => { console.log(results); });
+    }).catch((err) => console.log(err));
+}
+
+parseNewsToAlexaBrief();
